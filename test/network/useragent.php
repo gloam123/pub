c7e7dd79398c7cbc138740add98eba8a65ed1316
gloam <gloam@163.com>
2020-03-30 18:56:22 +0800

    test: input method viewport.

--- test/network/useragent.php
+++ test/network/useragent.php
@@ -4,16 +4,37 @@
 <meta name="renderer" content="ie-comp" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <title>UserAgent</title>
+<script>
+function onLoad() {
+	document.getElementById('idJsUa').innerHTML = navigator.userAgent;
+}
+</script>
+
 </head>
 
-<body>
+<body onload="onLoad()">
+Javascript UserAgent:
+<div id="idJsUa"></div>
+
 <br />
 <br />
+<br />
+
 <?php
-echo "<br/><br/>UserAgent:". $_SERVER['HTTP_USER_AGENT'] . "<br/><br/><br/>";
-echo "Accept:" . $_SERVER['HTTP_ACCEPT'];
+echo "Server UserAgent:<br/>". $_SERVER['HTTP_USER_AGENT'] . "<br/>";
+
+echo "<br/><br/><br/> Accept:" . $_SERVER['HTTP_ACCEPT'];
 ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" renderer="webkit" />
 <br />
 <br />
+<br />
+step="0.01"
+<form action="#">
+请输入数字2: <input type="number" step="0.01" />
+<input type="submit"  />
+<br />
+<br />
+</form>
 </body>
 </html>
